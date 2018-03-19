2d03d4b602166389b9da2ad31bfe4a064f80add1
Thanh Hải <hai2571998@gmail.com>
2018-03-19 19:21:17 +0700

    Set Up Menu and Navigation

---
+++ app/src/main/java/com/thanhhai/badminton/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.thanhhai.badminton;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    private View parentView;
+    private ResideMenu resideMenu;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        parentView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return parentView;
+    }
+}
---
+++ app/src/main/java/com/thanhhai/badminton/Slide_Navigation_Main.java
@@ -0,0 +1,125 @@
+package com.thanhhai.badminton;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+
+public class Slide_Navigation_Main extends FragmentActivity implements View.OnClickListener {
+
+    private ResideMenu resideMenu;
+    private Slide_Navigation_Main mContext;
+    private ResideMenuItem itemHome;
+    private ResideMenuItem itemProfile;
+    private ResideMenuItem itemCalendar;
+    private ResideMenuItem itemSettings;
+    private ResideMenuItem itemMessage;
+    private ResideMenuItem itemFriends;
+    private ResideMenuItem itemEvents;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slide_navigation_main);
+        mContext = this;
+        setUpMenu();
+        if (savedInstanceState == null)
+            changeFragment(new HomeFragment());
+    }
+
+    private void setUpMenu() {
+
+        // attach to current activity;
+        resideMenu = new ResideMenu(this);
+        resideMenu.setUse3D(true);
+        resideMenu.setBackground(R.drawable.menu_background);
+        resideMenu.attachToActivity(this);
+        resideMenu.setMenuListener(menuListener);
+        //valid scale factor is between 0.0f and 1.0f. leftmenu'width is 150dip. 
+        resideMenu.setScaleValue(1.0f);
+
+        // create menu items;
+        itemHome = new ResideMenuItem(this, R.drawable.ic_home_dark, "Trang chủ");
+        itemFriends = new ResideMenuItem(this, R.drawable.icon_videos, "Videos");
+        itemEvents = new ResideMenuItem(this, R.drawable.icon_aboutus, "Về chúng tôi");
+        itemSettings = new ResideMenuItem(this, R.drawable.icon_settings, "Cài đặt");
+
+        itemHome.setOnClickListener(this);
+        itemFriends.setOnClickListener(this);
+        itemEvents.setOnClickListener(this);
+        itemSettings.setOnClickListener(this);
+
+
+        resideMenu.addMenuItem(itemHome, ResideMenu.DIRECTION_LEFT);
+        resideMenu.addMenuItem(itemFriends, ResideMenu.DIRECTION_LEFT);
+        resideMenu.addMenuItem(itemSettings, ResideMenu.DIRECTION_LEFT);
+        resideMenu.addMenuItem(itemEvents, ResideMenu.DIRECTION_LEFT);
+
+        // You can disable a direction by setting ->
+        // resideMenu.setSwipeDirectionDisable(ResideMenu.DIRECTION_RIGHT);
+
+        findViewById(R.id.title_bar_left_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resideMenu.openMenu(ResideMenu.DIRECTION_LEFT);
+            }
+        });
+//        findViewById(R.id.title_bar_right_menu).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                resideMenu.openMenu(ResideMenu.DIRECTION_RIGHT);
+//            }
+//        });
+    }
+
+//    @Override
+//    public boolean dispatchTouchEvent(MotionEvent ev) {
+//        return resideMenu.dispatchTouchEvent(ev);
+//    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == itemHome) {
+            changeFragment(new HomeFragment());
+        } else if (view == itemFriends) {
+            changeFragment(new FriendsFragment());
+        } else if (view == itemEvents) {
+            changeFragment(new EventFragment());
+        } else if(view == itemSettings) {
+            changeFragment(new SettingsFragment());
+        }
+
+        resideMenu.closeMenu();
+    }
+
+    private ResideMenu.OnMenuListener menuListener = new ResideMenu.OnMenuListener() {
+        @Override
+        public void openMenu() {
+            //Toast.makeText(mContext, "Menu is opened!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void closeMenu() {
+            //Toast.makeText(mContext, "Menu is closed!", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private void changeFragment(Fragment targetFragment) {
+        resideMenu.clearIgnoredViewList();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment, targetFragment, "fragment")
+                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+    }
+
+    // What good method is to access resideMenu？
+    public ResideMenu getResideMenu() {
+        return resideMenu;
+    }
+}
